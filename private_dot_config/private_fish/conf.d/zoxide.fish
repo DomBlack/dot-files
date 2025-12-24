72bb59920849a350c1a45713a2892d8caf88c477
Dominic Black <dom@avianlabs.net>
2025-12-24 22:11:53 +0000

    Add zoxide configuration
    This commit adds the configuration for zoxide to the fish shell.

---
+++ private_dot_config/private_fish/conf.d/zoxide.fish
@@ -0,0 +1,6 @@
+# Zoxide - smarter cd that learns your habits
+# z foo     - jump to directory matching "foo"
+# zi foo    - interactive selection with fzf
+# z foo bar - jump to directory matching "foo" and "bar"
+zoxide init fish | source
+
